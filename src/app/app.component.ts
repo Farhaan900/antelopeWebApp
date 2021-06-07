e6fcd0baffce2cbba8d891d0783bea0560599535
vamsi mathala <matalav55@gmail.com>
2021-06-07 20:32:58 +0530

    Changed name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import * as skrollr from 'skrollr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'antelope-dao-app';
+
+  public ngOnInit(): void {
+      skrollr.init();
+  }
+  // public ngAfterViewInit() : void{
+  //     skrollr.init().refresh();
+  // }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -13,7 +14,6 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AboutComponent } from './about/about.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
 import { TransactionsTestComponent } from './transactions-test/transactions-test.component';
 import { RulerTokenComponent } from './ruler-token/ruler-token.component';
 import { EngineeringComponent } from './engineering/engineering.component';
@@ -38,7 +38,8 @@ import { SocialLinksComponent } from './social-links/social-links.component';
     TeamComponent,
     CommunityComponent,
     FooterComponent,
-    SocialLinksComponent
+    SocialLinksComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -46,7 +47,6 @@ import { SocialLinksComponent } from './social-links/social-links.component';
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    AnimateOnScrollModule.forRoot(),
     RouterModule.forRoot([
       {path: 'home', component: HomeComponent},
       {path: 'about', component: AboutComponent},
