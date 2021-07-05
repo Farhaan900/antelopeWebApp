7afb415cfff9c76ef50b14971a6920d1a4e374d2
vamsi mathala <matalav55@gmail.com>
2021-07-05 22:21:40 +0530

    finished home

--- src/app/social-links/social-links.component.ts
+++ src/app/social-links/social-links.component.ts
@@ -9,35 +9,6 @@ export class SocialLinksComponent implements OnInit {
 
   @Input() spacing: string | undefined;
 
-  colorRed: string = "20px";
-
-  socialLinks = [
-    {
-      imageName: "Linkedin.png",
-      link: "https://www.linkedin.com/company/smacoteq-it"
-    },
-    {
-      imageName: "Twitter.png",
-      link: "https://twitter.com/ANTELOPEDAO"
-    }, 
-    {
-      imageName: "Discord.png",
-      link: "https://discord.gg/ZUK59p79aG"
-    }, 
-    {
-      imageName: "Reddit.png",
-      link: "#"
-    }, 
-    {
-      imageName: "Telegram.png",
-      link: "https://t.me/joinchat/fZil3r7YwaI3ZGQ0"
-    }, 
-    {
-      imageName: "Youtube.png",
-      link: "#"
-    },
-  ]
-
   constructor() { }
 
   ngOnInit(): void {
