2db0d19f7f5fd964a5c57c5eeb64075b7361326c
vamsi mathala <matalav55@gmail.com>
2021-07-12 12:34:39 +0530

    modified changes

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Globals {
   name: string = 'vamsi';
-  language: string = 'Fr';
+  language: string = 'En';
   theme: string = 'light'
 }
